3d02955e289be21e9172a31d743b524ff379260c
Sezer Burkaş <sezerburkas@gmail.com>
2021-08-08 15:17:02 +0100

    Update varSum.java

--- Java/partOne/varSum.java
+++ Java/partOne/varSum.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 import javax.lang.model.util.ElementScanner6;
 public class varSum {
     public static void main(String[] args){
-
+        
         //System.in is a standart input system
-        Scanner scan = new Scanner(System.in); 
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Please enter first number = ");
         int num1 = scan.nextInt();
         System.out.print("Please enter second number = ");
         int num2 = scan.nextInt();
 
-        if (num1 > num2){
-            System.out.print("First number is bigger");
-        }else if (num1 < num2){
-            System.out.print("Second number is bigger");
-        }else{
-            System.out.print("Both numbers are equal");
-        }
+        int sum = num1 + num2;
+        System.out.print("Result = "+sum);
 
         
     }
